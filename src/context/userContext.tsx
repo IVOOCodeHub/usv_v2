fddcc6758d098e302f10a1df39e6c19b614bb9b2
JaRoD41 <laurent.trinco@gmail.com>
2024-11-26 17:28:14 +0100

    début tests ajout components dans page

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -1,64 +1,56 @@
 // types
-import { ReactElement } from "react";
-import { IUser, IUserCredentials } from "../utils/types/user.interface.ts";
+// import { ReactElement } from "react";
+// hooks | libraries
+import { createContext, useState, Context, ReactElement } from 'react'
+import { IUser, IUserCredentials } from '../utils/types/user.interface.ts'
 interface IUserContext {
-  user: IUser | null;
-  setUser: (user: IUser | null) => void;
-  getUser: (userCredentials: IUserCredentials) => Promise<void>;
-  credentialsErrorMessage: string;
-  setCredentialsErrorMessage: (errorMessage: string) => void;
+	user: IUser | null
+	setUser: (user: IUser | null) => void
+	getUser: (userCredentials: IUserCredentials) => Promise<void>
+	credentialsErrorMessage: string
+	setCredentialsErrorMessage: (errorMessage: string) => void
 }
 
-// hooks | libraries
-import { createContext, useState, Context } from "react";
-
 // services
-import { getUserService } from "../API/services/User.service.ts";
+import { getUserService } from '../API/services/User.service.ts'
 
 export const UserContext: Context<IUserContext> = createContext<IUserContext>({
-  user: null,
-  setUser: (): void => {},
-  getUser: async (): Promise<void> => {},
-  credentialsErrorMessage: "",
-  setCredentialsErrorMessage: (): void => {},
-});
+	user: null,
+	setUser: (): void => {},
+	getUser: async (): Promise<void> => {},
+	credentialsErrorMessage: '',
+	setCredentialsErrorMessage: (): void => {},
+})
 
-export const UserProvider = ({
-  children,
-}: {
-  children: ReactElement;
-}): ReactElement => {
-  const [user, setUser] = useState<IUser | null>(null);
-  const [credentialsErrorMessage, setCredentialsErrorMessage] =
-    useState<string>("");
+export const UserProvider = ({ children }: { children: ReactElement }): ReactElement => {
+	const [user, setUser] = useState<IUser | null>(null)
+	const [credentialsErrorMessage, setCredentialsErrorMessage] = useState<string>('')
 
-  const getUser: (userCredentials: IUserCredentials) => Promise<void> = async (
-    userCredentials: IUserCredentials,
-  ): Promise<void> => {
-    // add a timer to avoid flashing du to loader if datas fetch is instant
-    await new Promise((resolve: (value: unknown) => void): number =>
-      setTimeout(resolve, 500),
-    );
+	const getUser: (userCredentials: IUserCredentials) => Promise<void> = async (
+		userCredentials: IUserCredentials
+	): Promise<void> => {
+		// add a timer to avoid flashing du to loader if datas fetch is instant
+		await new Promise((resolve: (value: unknown) => void): number => setTimeout(resolve, 500))
 
-    const res: IUser | string = await getUserService(userCredentials);
-    if (typeof res === "string") {
-      setCredentialsErrorMessage(res);
-    } else {
-      setUser(res);
-    }
-  };
+		const res: IUser | string = await getUserService(userCredentials)
+		if (typeof res === 'string') {
+			setCredentialsErrorMessage(res)
+		} else {
+			setUser(res)
+		}
+	}
 
-  return (
-    <UserContext.Provider
-      value={{
-        user,
-        setUser,
-        getUser,
-        credentialsErrorMessage,
-        setCredentialsErrorMessage,
-      }}
-    >
-      {children}
-    </UserContext.Provider>
-  );
-};
+	return (
+		<UserContext.Provider
+			value={{
+				user,
+				setUser,
+				getUser,
+				credentialsErrorMessage,
+				setCredentialsErrorMessage,
+			}}
+		>
+			{children}
+		</UserContext.Provider>
+	)
+}
--- src/views/commandes/comptabilite/tresorerie/nouvellePrevisionDepenses/NouvellePrevisionDepenses.tsx
+++ src/views/commandes/comptabilite/tresorerie/nouvellePrevisionDepenses/NouvellePrevisionDepenses.tsx
@@ -1,24 +1,42 @@
 import './nouvellePrevisionDepenses.scss'
 
 // hooks | libraries
-// import { useNavigate, NavigateFunction } from 'react-router-dom'
+import { useNavigate, NavigateFunction } from 'react-router-dom'
 // import { NRTL } from 'nillys-react-table-library'
-// import { useEffect, useState, useContext, ReactElement } from 'react'
+import { useEffect, useState, useContext, ReactElement } from 'react'
 
-// // components
-// import withAuth from '../../../../../views/auth/withAuth'
+// components
+import withAuth from '../../../../../views/auth/withAuth'
 import Header from '../../../../../components/header/Header'
-// import Loader from '../../../../../components/loader/Loader'
-// import Button from '../../../../../components/button/Button.tsx'
-// import Footer from '../../../../../components/footer/Footer'
+import DefinitionComponent from '../../../../../components/definition/DefinitionComponent.tsx'
+import Loader from '../../../../../components/loader/Loader'
+import Button from '../../../../../components/button/Button.tsx'
+import Footer from '../../../../../components/footer/Footer'
 
-// // context
-// import { LoaderContext } from '../../../../../context/loaderContext.tsx'
-// import { UserContext } from '../../../../../context/userContext'
-// import { CourrierContext } from '../../../../../context/courrierContext.tsx'
-// import { ICourrierDepenses } from '../../../../../utils/types/courrier.interface.ts'
+// context
+import { LoaderContext } from '../../../../../context/loaderContext.tsx'
+import { UserContext } from '../../../../../context/userContext'
+import { CourrierContext } from '../../../../../context/courrierContext.tsx'
+import { ICourrierDepenses } from '../../../../../utils/types/courrier.interface.ts'
 
 const NouvellePrevisionDepenses = () => {
+	const navigate: NavigateFunction = useNavigate()
+	const { isLoading, startLoading, stopLoading } = useContext(LoaderContext)
+	const { user } = useContext(UserContext)
+	const { courrierDepenses, getCourrierDepenses } = useContext(CourrierContext)
+
+  const items = [
+		{ label: 'Date pièce', value: '01/01/2024' },
+		{ label: 'Société', value: 'OpenAI Inc.' },
+		{ label: 'Rubrique', value: null },
+		{ label: 'Montant TTC', value: '1234.56 €' },
+		{ label: 'Banque règlement', value: undefined },
+		{ label: 'Avec TVA', value: 'Oui' },
+		{ label: 'Moins de 20% de TVA', value: 'Non' },
+		{ label: 'TVA 20%', value: '246.91 €' },
+		{ label: 'Date échéance', value: '31/12/2024' },
+		{ label: 'Date Ordo.', value: '15/12/2024' },
+	]
 	return (
 		<>
 			<Header
@@ -26,8 +44,19 @@ const NouvellePrevisionDepenses = () => {
 					pageURL: 'GIVOO | TRESORÉRIE | NOUVELLE PREVISION DÉPENSES',
 				}}
 			/>
-
-			<h1>Nouvelle Prévision Dépenses</h1>
+			<main id={'nouvellePrevisionDepenses'}>
+				<h1>Nouvelle Prévision Dépenses</h1>
+        <DefinitionComponent items={items} />
+				<Button
+					props={{
+						style: 'grey',
+						text: 'Retour',
+						type: 'button',
+						onClick: (): void => navigate('/commandes/tresorerie/menu'),
+					}}
+				/>
+			</main>
+			<Footer />
 		</>
 	)
 }
