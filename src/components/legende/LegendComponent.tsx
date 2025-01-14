6ad4b7b0a9d1819ac6413bf21d360135274a9d7e
JaRoD41 <laurent.trinco@gmail.com>
2025-01-14 15:37:37 +0100

    tableau classé par rubriques treso

--- src/components/legende/LegendComponent.tsx
+++ src/components/legende/LegendComponent.tsx
@@ -1,30 +1,59 @@
-// Mocking des données pour la légende (noms des cas séparés par des points-virgules)
-const mockedData = 'Cas 1;Cas 2;Cas 3;Cas 4;Cas 5'
+// // Mocking des données pour la légende (noms des cas séparés par des points-virgules)
+// const mockedData = 'Cas 1;Cas 2;Cas 3;Cas 4;Cas 5'
 
-// Transformation des données en un tableau d'objets avec une couleur aléatoire
-const mockedCases = mockedData.split(';').map((name) => ({
-	name: name.trim(),
-	color: getRandomColor(),
-}))
+// // Transformation des données en un tableau d'objets avec une couleur aléatoire
+// const mockedCases = mockedData.split(';').map((name) => ({
+// 	name: name.trim(),
+// 	color: getRandomColor(),
+// }))
 
-// Fonction pour générer une couleur aléatoire
-function getRandomColor() {
-	const letters = '0123456789ABCDEF'
-	let color = '#'
-	for (let i = 0; i < 6; i++) {
-		color += letters[Math.floor(Math.random() * 16)]
-	}
-	return color
+// // Fonction pour générer une couleur aléatoire
+// function getRandomColor() {
+// 	const letters = '0123456789ABCDEF'
+// 	let color = '#'
+// 	for (let i = 0; i < 6; i++) {
+// 		color += letters[Math.floor(Math.random() * 16)]
+// 	}
+// 	return color
+// }
+
+// // Composant LegendComponent
+// const LegendComponent = () => {
+// 	return (
+// 		<div className='Legend'>
+// 			<div className='Legend__container'>
+// 				<div className='Legend__header'>Légende</div>
+// 				<ul className='Legend__list'>
+// 					{mockedCases.map((cas) => (
+// 						<li key={cas.name} className='Legend__item'>
+// 							<span className='Legend__color-dot' style={{ backgroundColor: cas.color }}></span>
+// 							{cas.name}
+// 						</li>
+// 					))}
+// 				</ul>
+// 			</div>
+// 		</div>
+// 	)
+// }
+
+// export default LegendComponent
+
+interface LegendItem {
+	name: string
+	color: string
 }
 
-// Composant LegendComponent
-const LegendComponent = () => {
+interface LegendComponentProps {
+	cases: LegendItem[]
+}
+
+const LegendComponent = ({ cases }: LegendComponentProps) => {
 	return (
 		<div className='Legend'>
 			<div className='Legend__container'>
 				<div className='Legend__header'>Légende</div>
 				<ul className='Legend__list'>
-					{mockedCases.map((cas) => (
+					{cases.map((cas) => (
 						<li key={cas.name} className='Legend__item'>
 							<span className='Legend__color-dot' style={{ backgroundColor: cas.color }}></span>
 							{cas.name}
--- src/views/commandes/comptabilite/tresorerie/previsionAValider/PrevisionAValider.tsx
+++ src/views/commandes/comptabilite/tresorerie/previsionAValider/PrevisionAValider.tsx
@@ -1,4 +1,3 @@
-import './previsionAValider.scss'
 import { ReactElement, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { IPrevision } from '../../../../../utils/types/prevision.interface.ts'
@@ -7,6 +6,7 @@ import NRTL from '../../../../../components/NRTL/NRTL'
 import Button from '../../../../../components/button/Button.tsx'
 import Footer from '../../../../../components/footer/Footer'
 import DateRange from '../../../../../components/dateRange/DateRange'
+import './previsionAValider.scss'
 
 const PrevisionAValider: () => ReactElement = (): ReactElement => {
 	const navigate = useNavigate()
@@ -21,6 +21,10 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 			libelleCompteTiers: 'DESLANDES',
 			libelleEcriture: 'HYGIENE 11 4124 SOLDE CDE N° 557',
 			credit: '1000.0',
+			codeJournal: 'PREV',
+			datePiece: '2024-01-01',
+			rubriqueTreso: 'BANQUE', // Randomly assigned
+			nomFichier: '2023_01\\20230106_17_03_54.pdf',
 		},
 		{
 			cle: 'PREV002',
@@ -30,6 +34,10 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 			libelleCompteTiers: 'IONOS',
 			libelleEcriture: 'INFORMATIQUE 06 2T23 SOLDE CDE N° 273',
 			credit: '500.0',
+			codeJournal: 'PREV',
+			datePiece: '2024-02-01',
+			rubriqueTreso: 'NOTE DE FRAIS', // Randomly assigned
+			nomFichier: '2023_01\\20230106_17_03_54.pdf',
 		},
 		{
 			cle: 'PREV003',
@@ -39,6 +47,10 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 			libelleCompteTiers: 'SODEXO',
 			libelleEcriture: 'TR 11 4T24 SOLDE CDE N° 556',
 			credit: '1500.0',
+			codeJournal: 'PREV',
+			datePiece: '2024-03-01',
+			rubriqueTreso: 'FOURNISSEURS', // Randomly assigned
+			nomFichier: undefined, // Randomly assigned or left empty
 		},
 		{
 			cle: 'PREV004',
@@ -48,6 +60,10 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 			libelleCompteTiers: 'GANDI SAS',
 			libelleEcriture: 'INFORMATIQUE 08 3T23 SOLDE CDE N° 300',
 			credit: '750.0',
+			codeJournal: 'PREV',
+			datePiece: '2024-04-01',
+			rubriqueTreso: 'TELECOM', // Randomly assigned
+			nomFichier: '2023_01\\20230106_17_03_54.pdf',
 		},
 		{
 			cle: 'PREV005',
@@ -57,6 +73,10 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 			libelleCompteTiers: 'OCTOPUSH',
 			libelleEcriture: 'INFORMATIQUE 09 3T23 SOLDE CDE N° 318',
 			credit: '1200.0',
+			codeJournal: 'PREV',
+			datePiece: '2024-05-01',
+			rubriqueTreso: 'LOVERS', // Randomly assigned
+			nomFichier: '2023_01\\20230106_17_03_54.pdf',
 		},
 		{
 			cle: 'PREV006',
@@ -66,6 +86,10 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 			libelleCompteTiers: 'ALIEXPRESS',
 			libelleEcriture: 'INFORMATIQUE 10 4T24 SOLDE CDE N° 534',
 			credit: '900.0',
+			codeJournal: 'PREV',
+			datePiece: '2024-06-01',
+			rubriqueTreso: 'HONORAIRES', // Randomly assigned
+			nomFichier: undefined, // Randomly assigned or left empty
 		},
 		{
 			cle: 'PREV007',
@@ -75,6 +99,10 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 			libelleCompteTiers: 'AMAZON',
 			libelleEcriture: 'DOCUMENTATION 11 4T24 SOLDE CDE N° 546',
 			credit: '2000.0',
+			codeJournal: 'PREV',
+			datePiece: '2024-07-01',
+			rubriqueTreso: 'ELECTRICITE-EAU', // Randomly assigned
+			nomFichier: '2023_01\\20230106_17_03_54.pdf',
 		},
 		{
 			cle: 'PREV008',
@@ -84,6 +112,10 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 			libelleCompteTiers: 'AMAZON',
 			libelleEcriture: 'DOCUMENTATION 11 4T24 SOLDE CDE N° 546',
 			credit: '2000.0',
+			codeJournal: 'PREV',
+			datePiece: '2024-08-01',
+			rubriqueTreso: 'CHARGES SALARIALES', // Randomly assigned
+			nomFichier: '2023_01\\20230106_17_03_54.pdf',
 		},
 		{
 			cle: 'PREV009',
@@ -93,6 +125,10 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 			libelleCompteTiers: 'DESLANDES',
 			libelleEcriture: 'HYGIENE 11 4124 SOLDE CDE N° 557',
 			credit: '1000.0',
+			codeJournal: 'PREV',
+			datePiece: '2024-09-01',
+			rubriqueTreso: 'IMPOTS ET TAXES', // Randomly assigned
+			nomFichier: undefined, // Randomly assigned or left empty
 		},
 		{
 			cle: 'PREV010',
@@ -102,6 +138,10 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 			libelleCompteTiers: 'IONOS',
 			libelleEcriture: 'INFORMATIQUE 06 2T23 SOLDE CDE N° 273',
 			credit: '500.0',
+			codeJournal: 'PREV',
+			datePiece: '2024-10-01',
+			rubriqueTreso: 'ETALEMENT', // Randomly assigned
+			nomFichier: undefined, // Randomly assigned or left empty
 		},
 		{
 			cle: 'PREV011',
@@ -111,6 +151,10 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 			libelleCompteTiers: 'SODEXO',
 			libelleEcriture: 'TR 11 4T24 SOLDE CDE N° 556',
 			credit: '1500.0',
+			codeJournal: 'PREV',
+			datePiece: '2024-11-01',
+			rubriqueTreso: 'FOURNISSEURS', // Randomly assigned
+			nomFichier: undefined, // Randomly assigned or left empty
 		},
 		{
 			cle: 'PREV012',
@@ -120,6 +164,10 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 			libelleCompteTiers: 'GANDI SAS',
 			libelleEcriture: 'INFORMATIQUE 08 3T23 SOLDE CDE N° 300',
 			credit: '750.0',
+			codeJournal: 'PREV',
+			datePiece: '2024-12-01',
+			rubriqueTreso: 'TELECOM', // Randomly assigned
+			nomFichier: '2023_01\\20230106_17_03_54.pdf',
 		},
 		{
 			cle: 'PREV013',
@@ -129,6 +177,10 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 			libelleCompteTiers: 'OCTOPUSH',
 			libelleEcriture: 'INFORMATIQUE 09 3T23 SOLDE CDE N° 318',
 			credit: '1200.0',
+			codeJournal: 'PREV',
+			datePiece: '2025-01-01',
+			rubriqueTreso: 'LOVERS', // Randomly assigned
+			nomFichier: undefined, // Randomly assigned or left empty
 		},
 		{
 			cle: 'PREV014',
@@ -138,6 +190,10 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 			libelleCompteTiers: 'ALIEXPRESS',
 			libelleEcriture: 'INFORMATIQUE 10 4T24 SOLDE CDE N° 534',
 			credit: '900.0',
+			codeJournal: 'PREV',
+			datePiece: '2025-02-01',
+			rubriqueTreso: 'HONORAIRES', // Randomly assigned
+			nomFichier: '2023_01\\20230106_17_03_54.pdf',
 		},
 		{
 			cle: 'PREV015',
@@ -147,6 +203,10 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 			libelleCompteTiers: 'AMAZON',
 			libelleEcriture: 'DOCUMENTATION 11 4T24 SOLDE CDE N° 546',
 			credit: '2000.0',
+			codeJournal: 'PREV',
+			datePiece: '2025-03-01',
+			rubriqueTreso: 'ELECTRICITE-EAU', // Randomly assigned
+			nomFichier: '2023_01\\20230106_17_03_54.pdf',
 		},
 		{
 			cle: 'PREV016',
@@ -156,15 +216,10 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 			libelleCompteTiers: 'AMAZON',
 			libelleEcriture: 'DOCUMENTATION 11 4T24 SOLDE CDE N° 546',
 			credit: '2000.0',
-		},
-		{
-			cle: 'PREV017',
-			societe: 'Société N',
-			dateSaisie: '2025-05-01',
-			dateEcheance: '2025-05-15',
-			libelleCompteTiers: 'DESLANDES',
-			libelleEcriture: 'HYGIENE 11 4124 SOLDE CDE N° 557',
-			credit: '1000.0',
+			codeJournal: 'PREV',
+			datePiece: '2025-04-01',
+			rubriqueTreso: 'CHARGES SALARIALES', // Randomly assigned
+			nomFichier: undefined,
 		},
 	]
 
@@ -173,31 +228,39 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 	const [codeFilter, setCodeFilter] = useState('') // Only "Code" filter
 
 	// Convert data for the table
-	const convertToArray = (datas: IPrevision[]): string[][] =>
-		datas
-			.filter(
-				(data) => !codeFilter || data.cle.toLowerCase().includes(codeFilter.toLowerCase()) // Case-insensitive filter
-			)
-			.map((data) => [
-				data.societe || 'Non défini', // Société
-				data.cle || 'Non défini', // Code
-				data.dateSaisie || 'Non défini', // Date Saisie
-				data.dateEcheance || 'Non défini', // Date Échéance
-				data.libelleCompteTiers || 'Non défini', // Fournisseur
-				data.libelleEcriture || 'Non défini', // Libellé écriture
-				data.credit ? parseFloat(data.credit).toFixed(2) : '0.00', // Montant (parse string to number)
-			])
+	const convertToArrayWithGrouping = (datas: IPrevision[]): { rubrique: string; rows: (string | undefined)[][] }[] => {
+		const groupedData = datas.reduce(
+			(acc, data) => {
+				const rubrique = data.rubriqueTreso || 'Non défini'
+				if (!acc[rubrique]) {
+					acc[rubrique] = []
+				}
+				acc[rubrique].push([
+					data.societe || 'Non défini',
+					data.cle || 'Non défini',
+					data.dateSaisie || 'Non défini',
+					data.dateEcheance || 'Non défini',
+					data.libelleCompteTiers || 'Non défini',
+					data.libelleEcriture || 'Non défini',
+					data.credit ? parseFloat(data.credit).toFixed(2) : '0.00',
+					data.nomFichier || 'Aucun Fichier', // Ensure this is the last element
+				])
+				return acc
+			},
+			{} as Record<string, (string | undefined)[][]>
+		)
+
+		return Object.entries(groupedData).map(([rubrique, rows]) => ({
+			rubrique,
+			rows,
+		}))
+	}
 
 	// Update table data with mocked data
+	// Update table data with mocked data
 	useEffect((): void => {
-		setBodyArray(convertToArray(mockedPrevisions))
+		setBodyArray(convertToArrayWithGrouping(mockedPrevisions))
 	}, [codeFilter])
-
-	// Prepare table data
-	const tableData = {
-		tableHead: ['Société', 'Code', 'Date Saisie', 'Date Échéance', 'Fournisseur', 'Libellé écriture', 'Montant'],
-		tableBody: bodyArray,
-	}
 
 	return (
 		<>
@@ -213,37 +276,67 @@ const PrevisionAValider: () => ReactElement = (): ReactElement => {
 								type='text'
 								placeholder='Filtrer par code'
 								value={codeFilter}
-								onChange={(e) => setCodeFilter(e.target.value)} // Update code filter
+								onChange={(e) => setCodeFilter(e.target.value)}
 							/>
 						</div>
 					</div>
-					<NRTL
-						datas={tableData}
-						headerBackgroundColor='linear-gradient(to left, #84CDE4FF, #1092B8)'
-						headerHoverBackgroundColor='#1092B8'
-						showPreviousNextButtons
-						enableColumnSorting
-						showItemsPerPageSelector
-						showPagination
-						itemsPerPageOptions={[5, 25, 50]}
-						filterableColumns={[true, false, false, false, false, false, false]}
-						language='fr'
-						onRowClick={(index: number, rowData?: string[]) => {
-							if (rowData) {
-								// Create an object with field names as keys and row data as values
-								const rowDetails = tableData.tableHead.reduce(
-									(acc, field, i) => {
-										acc[field] = rowData[i] // Assign each field name to its corresponding value
-										return acc
-									},
-									{} as Record<string, string>
-								)
+					<div id='groupedTable'>
+						{convertToArrayWithGrouping(mockedPrevisions).map((group, groupIndex) => (
+							<div key={groupIndex} className='rubriqueGroup'>
+								<div className='rubriqueTitle'>{group.rubrique}</div>
+								<NRTL
+									datas={{
+										tableHead: [
+											'Société',
+											'Code',
+											'Date Saisie',
+											'Date Échéance',
+											'Fournisseur',
+											'Libellé écriture',
+											'Montant',
+											'Courrier',
+										],
+										tableBody: group.rows,
+									}}
+									headerBackgroundColor='linear-gradient(to left, #84CDE4FF, #1092B8)'
+									headerHoverBackgroundColor='#1092B8'
+									showPreviousNextButtons
+									enableColumnSorting
+									showItemsPerPageSelector
+									showPagination
+									itemsPerPageOptions={[5, 25, 50]}
+									filterableColumns={[true, false, false, false, false, false, false]}
+									language='fr'
+									onRowClick={(index: number, rowData?: (string | undefined)[]) => {
+										if (rowData) {
+											// Find the corresponding prevision via "cle"
+											const cle = rowData[1] // The second field is the unique key
+											const matchedPrevision = mockedPrevisions.find((prevision) => prevision.cle === cle)
 
-								console.log('Clicked Row Details:', rowDetails) // Log the row details to the console
-								// navigate(`/details_prevision_valider/${rowData[1]}`) // Navigate to details using Code
-							}
-						}}
-					/>
+											if (matchedPrevision) {
+												const rowDetails = {
+													societe: matchedPrevision.societe || 'Non défini',
+													cle: matchedPrevision.cle || 'Non défini',
+													dateSaisie: matchedPrevision.dateSaisie || 'Non défini',
+													dateEcheance: matchedPrevision.dateEcheance || 'Non défini',
+													fournisseur: matchedPrevision.libelleCompteTiers || 'Non défini',
+													libelleEcriture: matchedPrevision.libelleEcriture || 'Non défini',
+													montant: matchedPrevision.credit ? parseFloat(matchedPrevision.credit).toFixed(2) : '0.00',
+													rubriqueTreso: matchedPrevision.rubriqueTreso || 'Non défini',
+													nomFichier: matchedPrevision.nomFichier || 'Aucun fichier',
+												}
+
+												console.log('Clicked Row Details:', rowDetails)
+												// navigate(`/details_prevision_valider/${cle}`, { state: { fullRowDetails: rowDetails } })
+											} else {
+												console.error('Aucune prévision correspondante trouvée pour la clé:', cle)
+											}
+										}
+									}}
+								/>
+							</div>
+						))}
+					</div>
 					<div className='greyButtonWrapper'>
 						<Button props={{ style: 'grey', text: 'Retour', type: 'button', onClick: () => navigate(-1) }} />
 					</div>
