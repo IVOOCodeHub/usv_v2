8f894f7ad513700504089db0aea37aa114b08235
ChristelleChaussivert <chaussivertchristelle@gmail.com>
2025-02-07 11:04:48 +0100

    Mig Commandes fournisseurs
    Migration des pages :
    - commandes à valider
    - modification de commande
    - télécharger pièce
    - Voir pièce
    - imprimer
    - ajouter un article
    - modifier un article

--- src/views/commandes/commandesFournisseurs/commandesFournisseursRouter.tsx
+++ src/views/commandes/commandesFournisseurs/commandesFournisseursRouter.tsx
@@ -9,6 +9,9 @@ import CommandesModifWithAuth from "./commandesModif/commandesModif.tsx";
 
 
 import {UtilitaireCommandesWithAuth} from "./utilitaireCommandes/UtilitaireCommandes.tsx";
+import VoirPiecesWithAuth from "./voirPieces/voirPieces.tsx"
+import AjouterPiecesWithAuth from "./ajouterPiece/ajouterPiece.tsx";
+import ModifierPieceWithAuth from "./modifierPiece/modifierPiece.tsx";
 
 export default function CommandesFournisseursRouter() : ReactElement {
     return (
@@ -16,6 +19,10 @@ export default function CommandesFournisseursRouter() : ReactElement {
             <Route path={"/creation_commandes"} element={<CreationCommandesWithAuth/>}/>
             <Route path={"/commandes_a_valider"} element={<CommandesAValiderWithAuth/>}/>
             <Route path={"/commandes_a_valider/:commandeID"} element={<CommandesModifWithAuth/>}/>
+            <Route path={"/commandes_a_valider/voir_pieces/:commandeID"} element={<VoirPiecesWithAuth/>}/>
+            <Route path={"/commandes_a_valider/ajouter_piece/:commandeID"} element={<AjouterPiecesWithAuth/>}/>
+            <Route path={"/commandes_a_valider/modifier_piece/:commandeID"} element={<ModifierPieceWithAuth/>}/>
+
 
             <Route path={'/utilitaire_commandes'} element={<UtilitaireCommandesWithAuth />} />
         </Routes>
--- src/views/commandes/commandesFournisseurs/commandesModif/commandesModif.tsx
+++ src/views/commandes/commandesFournisseurs/commandesModif/commandesModif.tsx
@@ -1,9 +1,8 @@
 // styles
 import "./commandesModif.scss";
 
-
 // hooks | libraries
-import {ReactElement, useState} from "react";
+import {ReactElement, useEffect, useState} from "react";
 import { useNavigate, NavigateFunction } from "react-router-dom";
 
 // components
@@ -12,12 +11,15 @@ import Button from "../../../../components/button/Button";
 import Select from "react-select";
 import Footer from "../../../../components/footer/Footer";
 import WithAuth from "../../../auth/WithAuth";
+import NRTL from "../../../../components/NRTL/NRTL.tsx";
 
 function CommandesModif(): ReactElement {
     type OptionType = { value: number; label: string };
 
     const navigate: NavigateFunction = useNavigate();
+    const [bodyArray, setBodyArray] = useState<string[][]>([]);
     const [isReadOnly] = useState(true);
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
     const dataSociete1 = {
         id: "62639",
         societe: "IVOO",
@@ -35,13 +37,39 @@ function CommandesModif(): ReactElement {
         { value: 1, label: "Nelly" },
         { value: 2, label: "etc" },
     ];
+    useEffect((): void => {
+        const mockupBody: string[][] = [
+            [
+                "1",
+                "Pizza",
+                "50",
+                "21.00",
+                "0.00",
+                "21.00",
+            ],
+            [
+                "2",
+                "Trombones",
+                "100",
+                "11.00",
+                "0.00",
+                "21.00",
+            ],
+        ];
+        setBodyArray(mockupBody);
+    }, []);
+
+    const tableData = {
+        tableHead: ["Ref. article", "Description",  "Qté", "PU HT", "Taux TVA", "PU TTC"],
+        tableBody: bodyArray,
+    };
 
     return (
         <>
             <Header
                 props={{
                     pageURL:
-                        "G_IVOO | Commandes Fournisseurs | Création Commandes",
+                        "G_IVOO | Commandes Fournisseurs | Modification Commandes",
                     helpBtn: true,
                 }}
             />
@@ -133,7 +161,7 @@ function CommandesModif(): ReactElement {
                                     }}
                                 /><Button
                                     props={{
-                                        style: "green",
+                                        style: "blue",
                                         text: "Accepter",
                                         type: "button",
                                     }}
@@ -151,11 +179,151 @@ function CommandesModif(): ReactElement {
                                 />
                             </div>
                         </form>
-
+                        {isPopupOpen && (
+                            <div className="popupOverlay" id="updateServiceModal">
+                                <form className={"updateServiceForm"}>
+                                    <div className="popupContent">
+                                        <h2>Télécharger une pièce pour la commande {dataSociete1.id}</h2>
+                                        <div className={"formWrapper"}>
+                                            <div className={"inputWrapper"}>
+                                                <label htmlFor={"type"}>Type de document :</label>
+                                                <Select
+                                                    id={"type"}
+                                                    options={[
+                                                        { value: "1", label: "Devis" },
+                                                        { value: "2", label: "Courrier" },
+                                                        { value: "3", label: "Autre" },
+                                                    ]}
+                                                />
+                                            </div>
+                                        </div>
+                                        <input type="file" />
+                                        <div className={"buttonContainer"}>
+                                            <Button
+                                                props={{
+                                                    style: "green",
+                                                    text: "Ajouter",
+                                                    type: "button",
+                                                    onClick: (): void => setIsPopupOpen(false)
+                                                }}
+                                            />
+                                            <Button
+                                                props={{
+                                                    style: "grey",
+                                                    text: "Annuler",
+                                                    type: "button",
+                                                    onClick: (): void => setIsPopupOpen(false)
+                                                }}
+                                            />
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        )}
+                        <div className={"buttonContainer"}>
+                            <Button
+                                props={{
+                                    style: "red",
+                                    text: "Supprimer",
+                                    type: "button",
+                                    onClick: (): void =>
+                                        navigate(
+                                            "#",
+                                            // commandes_supprimer0.asp?code_commande=<%=code_commande%>
+                                        ),
+                                }}
+                            />
+                            <Button
+                            props={{
+                                style: "blue",
+                                text: "Télécharger pièce",
+                                type: "button",
+                                onClick: (): void => setIsPopupOpen(true),
+                                }}
+                            />
+                            <Button
+                                props={{
+                                    style: "blue",
+                                    text: "Voir pièces",
+                                    type: "button",
+                                    onClick: (): void =>
+                                        navigate(
+                                            `/commandes/commandes_fournisseurs/commandes_a_valider/voir_pieces/${dataSociete1.id}`,
+                                        ),
+                                }}
+                            />
+                            <Button
+                                props={{
+                                    style: "blue",
+                                    text: "Imprimer",
+                                    type: "button",
+                                    onClick: (): WindowProxy | null =>
+                                        window.open(
+                                            `http://srv-web:8081/CrystalWebViewerURL/view.aspx?mode=E&etat=commandes_f&xml=commandes_f&p1=so:${dataSociete1.societe}&p2=numBC:${dataSociete1.societe}`, '_blank'
+                                        ),
+                                }}
+                            />
+                            <Button
+                                props={{
+                                    style: "blue",
+                                    text: "Visu prévision",
+                                    type: "button",
+                                    onClick: (): void =>
+                                        navigate(
+                                            "#",
+                                        ),
+                                }}
+                            />
+                        </div>
                     </section>
                 </div>
-                <div className={"goBackBtnWrapper"}>
-
+                <NRTL
+                    datas={tableData}
+                    headerBackgroundColor={"linear-gradient(to left, #84CDE4FF, #1092B8)"}
+                    headerHoverBackgroundColor={"#1092B8"}
+                    showItemsPerPageSelector={true}
+                    showPreviousNextButtons={true}
+                    showSearchBar={true}
+                    onRowClick={(
+                        index: number,
+                        rowData: string[] | undefined,
+                    ): void =>
+                        navigate(
+                            `/commandes/commandes_fournisseurs/commandes_a_valider/modifier_piece/${rowData![0]}`,
+                            {
+                                state: { index: index, commandeID: rowData![0] },
+                            },
+                        )
+                    }
+                    filterableColumns={[true, true, true, true, true, true]}
+                    showPagination={true}
+                    enableColumnSorting={true}
+                    itemsPerPageOptions={[10, 25, 50]}
+                    language={"fr"}
+                />
+                <div className="buttonContainer" id={"bottom_buttons"}>
+                    <Button
+                        props={{
+                            style: "blue",
+                            text: "Ajouter une ligne d'article",
+                            type: "button",
+                            onClick: (): void =>
+                                navigate(
+                                    `/commandes/commandes_fournisseurs/commandes_a_valider/ajouter_piece/${dataSociete1.id}`,
+                                ),
+                        }}
+                    />
+                    <Button
+                        props={{
+                            style: "grey",
+                            text: "Retour",
+                            type: "button",
+                            onClick: (): void =>
+                                navigate(
+                                    "/commandes/commandes_fournisseurs",
+                                ),
+                        }}
+                    />
                 </div>
             </main>
             <Footer />
