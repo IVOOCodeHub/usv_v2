a15cceb8b425935fdae3cc843d8026bd836c8f7f
JaRoD41 <laurent.trinco@gmail.com>
2025-02-26 17:20:55 +0100

    ajout totaux équilibrages

--- src/views/commandes/comptabilite/tresorerie/equilibrages/Equilibrages.tsx
+++ src/views/commandes/comptabilite/tresorerie/equilibrages/Equilibrages.tsx
@@ -1,22 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Header from '../../../../../components/header/Header'
-import NRTL from '../../../../../components/NRTL/NRTL'
+import Nrtl from '../../../../../components/NRTL/NRTL'
 import Button from '../../../../../components/button/Button.tsx'
 import Footer from '../../../../../components/footer/Footer'
 import './equilibrages.scss'
-import { mockedEquilibrages } from './mock/mockedEquilibrages.ts'
+import { mockedEquilibrages } from './mock/mockedEquilibrages'
 
 interface RowDetails {
+	banque: string
+	dernierImport: string
+	compte: string
 	societe: string
-	cle: string
-	dateSaisie: string
-	dateEcheance: string
-	libelleCompteTiers: string
-	libelleEcriture: string
-	credit: string
-	debit: string
-	nomFichier?: string
 	dernierSolde: string
 	emisNonDecaisse: string
 	soldeApresEND: string
@@ -31,60 +26,71 @@ const Equilibrages: React.FC = () => {
 	const navigate = useNavigate()
 
 	// State for table data
-	const [bodyArray, setBodyArray] = useState<(string | undefined)[][]>([])
+	const [bodyArray, setBodyArray] = useState<string[][]>([])
+	const [totals, setTotals] = useState({
+		emisNonDecaisse: '0.00',
+		mobilisable: '0.00',
+		ordo: '0.00',
+		soldeApresOrdo: '0.00',
+	})
 
 	// Convert data for the table
-	const convertToArray = (datas: RowDetails[]): (string | undefined)[][] => {
+	const convertToArray = (datas: RowDetails[]): string[][] => {
 		return datas.map((data) => [
-			data.societe ?? 'Non défini', // Banque
-			data.cle || 'Non défini', // Code
-			data.dateSaisie ?? 'Non défini', // Date saisie
-			data.dateEcheance ?? 'Non défini', // Date échéance
-			data.libelleCompteTiers ?? 'Non défini', // Fournisseur
-			data.libelleEcriture ?? 'Non défini', // Libellé
-			data.credit ? parseFloat(data.credit).toFixed(2) : '0.00', // Montant
-			data.debit ? parseFloat(data.debit).toFixed(2) : '0.00', // Montant
-			data.nomFichier ?? 'Non défini', // Courrier
-			data.dernierSolde ?? 'Non défini', // Dernier Solde
-			data.emisNonDecaisse ?? 'Non défini', // Emis Non Décaissé
-			data.soldeApresEND ?? 'Non défini', // Solde après E.N.D.
-			data.decouvertAutorise ?? 'Non défini', // Découvert Autorisé
-			data.mobilisable ?? 'Non défini', // Mobilisable
-			data.ordo ?? 'Non défini', // Ordo
-			data.soldeApresOrdo ?? 'Non défini', // Solde après Ordo
-			data.soldeApresEqui ?? 'Non défini', // Solde après Equi
+			data.banque || 'Non défini', // Banque
+			data.dernierImport || 'Non défini', // Der.Import
+			data.compte || 'Non défini', // Compte
+			data.societe || 'Non défini', // Société
+			parseFloat(data.dernierSolde || '0').toFixed(2), // Dernier Solde
+			parseFloat(data.emisNonDecaisse || '0').toFixed(2), // Emis Non Décaissé
+			parseFloat(data.soldeApresEND || '0').toFixed(2), // Solde après E.N.D.
+			parseFloat(data.decouvertAutorise || '0').toFixed(2), // Découvert Autorisé
+			parseFloat(data.mobilisable || '0').toFixed(2), // Mobilisable
+			parseFloat(data.ordo || '0').toFixed(2), // Ordo
+			parseFloat(data.soldeApresOrdo || '0').toFixed(2), // Solde après Ordo
+			parseFloat(data.soldeApresEqui || '0').toFixed(2), // Solde après Equi
 		])
 	}
 
 	// Function to get row details
-	const getRowDetails = (cle: string): RowDetails | undefined => {
-		const matchedPrevision = mockedEquilibrages.find((prevision) => prevision.cle === cle)
+	const getRowDetails = (compte: string): RowDetails | undefined => {
+		const matchedPrevision = mockedEquilibrages.find((prevision) => prevision.compte === compte)
 		if (!matchedPrevision) return undefined
 
 		return {
+			banque: matchedPrevision.banque ?? 'Non défini',
+			dernierImport: matchedPrevision.dernierImport ?? 'Non défini',
+			compte: matchedPrevision.compte || 'Non défini',
 			societe: matchedPrevision.societe ?? 'Non défini',
-			cle: matchedPrevision.cle || 'Non défini',
-			dateSaisie: matchedPrevision.dateSaisie ?? 'Non défini',
-			dateEcheance: matchedPrevision.dateEcheance ?? 'Non défini',
-			libelleCompteTiers: matchedPrevision.libelleCompteTiers ?? 'Non défini',
-			libelleEcriture: matchedPrevision.libelleEcriture ?? 'Non défini',
-			credit: matchedPrevision.credit ? parseFloat(matchedPrevision.credit).toFixed(2) : '0.00',
-			debit: matchedPrevision.debit ? parseFloat(matchedPrevision.debit).toFixed(2) : '0.00',
-			nomFichier: matchedPrevision.nomFichier ?? 'Non défini',
-			dernierSolde: matchedPrevision.dernierSolde ?? 'Non défini',
-			emisNonDecaisse: matchedPrevision.emisNonDecaisse ?? 'Non défini',
-			soldeApresEND: matchedPrevision.soldeApresEND ?? 'Non défini',
-			decouvertAutorise: matchedPrevision.decouvertAutorise ?? 'Non défini',
-			mobilisable: matchedPrevision.mobilisable ?? 'Non défini',
-			ordo: matchedPrevision.ordo ?? 'Non défini',
-			soldeApresOrdo: matchedPrevision.soldeApresOrdo ?? 'Non défini',
-			soldeApresEqui: matchedPrevision.soldeApresEqui ?? 'Non défini',
+			dernierSolde: parseFloat(matchedPrevision.dernierSolde || '0').toFixed(2),
+			emisNonDecaisse: parseFloat(matchedPrevision.emisNonDecaisse || '0').toFixed(2),
+			soldeApresEND: parseFloat(matchedPrevision.soldeApresEND || '0').toFixed(2),
+			decouvertAutorise: parseFloat(matchedPrevision.decouvertAutorise || '0').toFixed(2),
+			mobilisable: parseFloat(matchedPrevision.mobilisable || '0').toFixed(2),
+			ordo: parseFloat(matchedPrevision.ordo || '0').toFixed(2),
+			soldeApresOrdo: parseFloat(matchedPrevision.soldeApresOrdo || '0').toFixed(2),
+			soldeApresEqui: parseFloat(matchedPrevision.soldeApresEqui || '0').toFixed(2),
 		}
 	}
 
 	// Update table data with mocked data
 	useEffect(() => {
 		setBodyArray(convertToArray(mockedEquilibrages))
+	}, [])
+
+	// Calculate totals
+	useEffect(() => {
+		const totals = {
+			emisNonDecaisse: mockedEquilibrages
+				.reduce((prev, curr) => prev + parseFloat(curr.emisNonDecaisse || '0'), 0)
+				.toFixed(2),
+			mobilisable: mockedEquilibrages.reduce((prev, curr) => prev + parseFloat(curr.mobilisable || '0'), 0).toFixed(2),
+			ordo: mockedEquilibrages.reduce((prev, curr) => prev + parseFloat(curr.ordo || '0'), 0).toFixed(2),
+			soldeApresOrdo: mockedEquilibrages
+				.reduce((prev, curr) => prev + parseFloat(curr.soldeApresOrdo || '0'), 0)
+				.toFixed(2),
+		}
+		setTotals(totals)
 	}, [])
 
 	return (
@@ -96,16 +102,14 @@ const Equilibrages: React.FC = () => {
 						{bodyArray.length === 0 ? (
 							<div className='no-results'>Aucune donnée disponible.</div>
 						) : (
-							<NRTL
+							<Nrtl
 								datas={{
 									tableHead: [
 										'Banque',
-										'Code',
-										'Date saisie',
-										'Échéance',
-										'Fournisseur',
-										'Libellé',
-										'Dernier Solde',
+										'Der.Import',
+										'Compte',
+										'Société',
+										'Dernier Solde Importé',
 										'Emis Non Décaissé',
 										'Solde après E.N.D.',
 										'Découvert Autorisé',
@@ -122,13 +126,13 @@ const Equilibrages: React.FC = () => {
 								enableColumnSorting
 								showItemsPerPageSelector
 								showPagination
-								itemsPerPageOptions={[5, 25, 50]}
-								filterableColumns={[false, false, false, false, true, false, false, false, false, false, false]}
+								itemsPerPageOptions={[10, 25]}
+								filterableColumns={[false, false, false, false, false, false, false, false, false, false, false]}
 								language='fr'
-								onRowClick={(index: number, rowData?: (string | undefined)[]) => {
-									if (rowData && rowData[1]) {
-										const cle = rowData[1]
-										const rowDetails = getRowDetails(cle)
+								onRowClick={(_, rowData?: string[]) => {
+									if (rowData?.[2]) {
+										const compte = rowData[2]
+										const rowDetails = getRowDetails(compte)
 										if (rowDetails) {
 											console.log('RowDetails :', rowDetails)
 
@@ -136,12 +140,18 @@ const Equilibrages: React.FC = () => {
 												state: { fullRowDetails: rowDetails },
 											})
 										} else {
-											console.error('Aucune prévision correspondante trouvée pour la clé:', cle)
+											console.error('Aucune prévision correspondante trouvée pour la clé:', compte)
 										}
 									}
 								}}
 							/>
 						)}
+						<div className='totals'>
+							<div>Total Emis Non Décaissé: {parseFloat(totals.emisNonDecaisse).toFixed(2)}</div>
+							<div>Total Mobilisable: {parseFloat(totals.mobilisable).toFixed(2)}</div>
+							<div>Total Ordo: {parseFloat(totals.ordo).toFixed(2)}</div>
+							<div>Total Solde après Ordo: {parseFloat(totals.soldeApresOrdo).toFixed(2)}</div>
+						</div>
 					</div>
 					<div className='greyButtonWrapper'>
 						<Button props={{ style: 'grey', text: 'Retour', type: 'button', onClick: () => navigate(-1) }} />
